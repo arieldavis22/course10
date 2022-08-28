7f2add1308c969ace8dca7027a428f2c081c01b0
arieldavis22 <amanichai100@gmail.com>
2022-08-27 23:49:40 -0400

    overview on collections

---
+++ src/CollectionsOverview/Main.java
@@ -0,0 +1,20 @@
+package CollectionsOverview;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theater theater = new Theater("Oly", 8, 12);
+//        theater.getSeats();
+
+        if(theater.reseveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Seat is taken");
+        }
+        if(theater.reseveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Seat is taken");
+        }
+    }
+}
